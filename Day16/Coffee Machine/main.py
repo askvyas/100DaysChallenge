0645a83aa781e70b6b47931ddb4e6e66e28b8913
askvyas <vyasakondi009@gmail.com>
2021-01-04 21:03:57 +0530

    CoffeMaker v1 Added

--- Day16/Coffee Machine/main.py
+++ Day16/Coffee Machine/main.py
@@ -13,15 +13,22 @@ def drink_coffee():
     user_inp = input("What would you like to have? ")
     if user_inp == "report":
         usr_coffee.report()
+        drink_coffee()
     elif user_inp == "off":
         return
     else:
         c_d=usr_menu.find_drink(user_inp)
         if c_d != None:
-            if usr_coffee.is_resource_sufficient(user_inp):
-                usr_mon.make_payment(c_d.cost)
+            if usr_coffee.is_resource_sufficient(c_d):
+                if usr_mon.make_payment(c_d.cost):
+                    usr_coffee.make_coffee(c_d)
+                    drink_coffee()
+        else:
+            drink_coffee()
 
 
+
+drink_coffee()
 
 
 
