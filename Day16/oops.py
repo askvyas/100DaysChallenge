2873dc5682704f54064faa7b856cd96b3d5af912
askvyas <vyasakondi009@gmail.com>
2021-01-04 12:02:33 +0530

    Basic Turtle Added

--- Day16/oops.py
+++ Day16/oops.py
@@ -3,5 +3,16 @@
 from turtle import Turtle, Screen
 timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+
 my_screen = Screen()
-print(my_screen.canvheight)
+# print(my_screen.canvheight)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+my_screen.exitonclick()
