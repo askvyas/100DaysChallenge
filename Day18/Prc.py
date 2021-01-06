b3e141e8ba0e0d7aba8e67d83b40232a821f29cc
askvyas <vyasakondi009@gmail.com>
2021-01-06 09:39:05 +0530

    Day 18 Start

---
+++ Day18/Prc.py
@@ -0,0 +1,23 @@
+from turtle import  Turtle,Screen
+
+timmy_the_turtle=Turtle()
+# timmy_the_turtle.shape("circle")
+# timmy_the_turtle.color("red")
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.dot(19)
+#
+# timmy_the_turtle.forward(100)
+#
+#Drawing a square using turtle
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
