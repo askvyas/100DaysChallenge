7d495e7aa3d603283af86beb1979276f9a631982
askvyas <vyasakondi009@gmail.com>
2021-01-06 10:04:33 +0530

    Prc file updated

--- Day18/Prc.py
+++ Day18/Prc.py
@@ -1,6 +1,9 @@
-from turtle import  Turtle,Screen
 
-timmy_the_turtle=Turtle()
+
+from turtle import *
+
+import random
+tim = Turtle()
 # timmy_the_turtle.shape("circle")
 # timmy_the_turtle.color("red")
 # timmy_the_turtle.forward(100)
@@ -9,15 +12,54 @@ timmy_the_turtle=Turtle()
 #
 # timmy_the_turtle.forward(100)
 #
-#Drawing a square using turtle
+# # Drawing a square using turtle
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# # Dashed line
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
 
+# shapes
+# # Triangle
+# tim.forward(100)
+# tim.right(120)
+# tim.forward(100)
+# tim.right(120)
+# tim.forward(100)
+# # Square
+# tim.right(120)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
 
-
-
-
-
-
-
+# to change color to random
+# def change_color(t):
+#     R = random.random()
+#     B = random.random()
+#     G = random.random()
+#
+#     return t.color(R,G,B)
+#
+#
+#
+# for i in range(3, 8):
+#     change_color(tim)
+#
+#     for _ in range(i):
+#         tim.forward(100)
+#         tim.right(360/i)
 
 screen = Screen()
 screen.exitonclick()
