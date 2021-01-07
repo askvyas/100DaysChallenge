355088a72ef8d1911ccfec56b7891673da46b941
askvyas <vyasakondi009@gmail.com>
2021-01-07 17:23:09 +0530

    Day19 Etchgame Added

--- Day19/Prc.py
+++ Day19/Prc.py
@@ -1,8 +1,8 @@
-
 from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
+
 
 # # Event Listeners
 # def move_forwards():
@@ -12,5 +12,40 @@ screen = Screen()
 # screen.listen()
 # screen.onkey(key="space",fun=move_forwards)
 
+# Etch a sketch using turtle
+# To Do: press "w" : Forwards
+#          press "s" : Backwards
+#          press "a" : Counter Clockwise
+#          press "d" : Clockwise
+#          press "c" : Clear Drawing
+# def move_forward():
+#     tim.forward(10)
+#
+#
+# def move_backwards():
+#     tim.backward(10)
+#
+#
+# def clkwise():
+#    # tim.left(90)
+#    new_heading = tim.heading()+10
+#    tim.setheading(new_heading)
+#
+#
+# def cnt_clkwise():
+#
+#     tim.right(10)
+#
+# def clear_g():
+#     tim.reset()
+#
+#
+#
+# screen.listen()
+# screen.onkey(key="w", fun=move_forward)
+# screen.onkey(key="s", fun=move_backwards)
+# screen.onkey(key="a", fun=clkwise)
+# screen.onkey(key="d", fun=cnt_clkwise)
+# screen.onkey(key="c", fun=clear_g)
 
 screen.exitonclick()
