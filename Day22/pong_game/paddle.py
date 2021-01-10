405fa7c04659f8c08825d8e753c68028a0c5aece
askvyas <vyasakondi009@gmail.com>
2021-01-10 22:52:25 +0530

    pong_game screen added

---
+++ Day22/pong_game/paddle.py
@@ -0,0 +1,15 @@
+# from turtle import Turtle
+#
+#
+# class Paddle(Turtle):
+#     def __int__(self):
+#         super(Paddle, self).__int__()
+#         self.shape("square")
+#         #self.color("white")
+#         self.shapesize(stretch_wid=5, stretch_len=1)
+#         self.go_to()
+#
+#     def go_to(self):
+#         xcor = -760
+#         ycor = 0
+#         self.goto(xcor, ycor)
