b9573ffc63d0e74a6ebadc1fd0deb4988bbf8c42
askvyas <vyasakondi009@gmail.com>
2021-01-11 15:01:39 +0530

    pong_game Complete

--- Day22/pong_game/main.py
+++ Day22/pong_game/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -17,27 +18,37 @@ screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
-
+score=Scoreboard()
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     # Detect the collision of the ball with top and bottom walls
     if ball.ycor() > 280 or ball.ycor() < -280:
         # It needs to bounce
+
         ball.bounce_y()
     # To detect collision with r_paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+
         print("Made Contact")
         ball.bounce_x()
 
     # Detect of Right paddle misses
     if ball.xcor() >380:
+        score.l_point()
+        score.update_scoreboard()
+
+
         ball.reset_position()
+
     if ball.xcor() < -380:
+        score.r_point()
+        score.update_scoreboard()
         ball.reset_position()
+        score.r_score += 1
 
 
 
