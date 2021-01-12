b10e78b5e958e7dc2f7063915c10e3359e623ad6
askvyas <vyasakondi009@gmail.com>
2021-01-12 17:33:23 +0530

    Day23 turtle_crossing turtle added

--- Day23/turtle_crossing/car_manager.py
+++ Day23/turtle_crossing/car_manager.py
@@ -1,7 +1,19 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super(CarManager, self).__init__()
+        self.shape("square")
+        self.shapesize(stretch_len=2,stretch_wid=1)
+        self.penup()
+    def gen_car(self):
+
+
+
+
--- Day23/turtle_crossing/main.py
+++ Day23/turtle_crossing/main.py
@@ -7,8 +7,12 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
+ply=Player()
+screen.listen()
+screen.onkey(ply.go_forward,"Up")
+car=CarManager()
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
