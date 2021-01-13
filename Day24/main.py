433a8c5d0a94079b198ae2296b300ea1fafc698d
askvyas <vyasakondi009@gmail.com>
2021-01-13 15:47:50 +0530

    Day 24 Prc complete

--- Day24/main.py
+++ Day24/main.py
@@ -12,13 +12,22 @@ with open("tasks.txt") as file:
 #     file.write("Written using files")
 #     print(contents)    //This deletes all the prev text inside the file
 
+#
+# with open("tasks.txt",mode='a') as file:
+#     file.write("\nWritten using files")
+#     print(contents)
+#
+# # Creating a file using w mode
+#
+# with open("new_file.txt",mode='w') as file:
+#     file.write("I Created a file using python")
 
-with open("tasks.txt",mode='a') as file:
-    file.write("\nWritten using files")
+# working with files and paths
+# # using absolute path
+# with open("/Users/askvyas/Desktop/hello.txt" ) as file:
+#     contents=file.read()
+#     print(contents)
+
+with open("../../../Desktop/hello.txt" ) as file:
+    contents=file.read()
     print(contents)
-
-# Creating a file using w mode
-
-with open("new_file.txt",mode='w') as file:
-    file.write("I Created a file using python")
-
--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -9,7 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score=0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.goto(0, 270)
         self.penup()
         self.speed(0)
@@ -17,17 +18,14 @@ class Scoreboard(Turtle):
         self.color("White")
         self.updat_score()
 
-
-
     def updat_score(self):
         self.clear()
-        self.write(f"Score : {self.score}  High Score {self.high_score}", move=False, align="center", font=("Arial", 20, "normal"))
+        self.write(f"Score : {self.score}  High Score {self.high_score}", move=False, align="center",
+                   font=("Arial", 20, "normal"))
 
     def increase_score(self):
         self.score += 1
         self.updat_score()
-
-
 
     # def game_over(self):
     #     self.goto(0, 0)
@@ -35,4 +33,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score=self.score
-        self.score=0
+            with open("data.txt", mode='w') as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.updat_score()
