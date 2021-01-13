0c1c41ee046d2343feb231afc1790dc8e074a03a
askvyas <vyasakondi009@gmail.com>
2021-01-13 15:11:54 +0530

    File I/O Added

---
+++ Day24/main.py
@@ -0,0 +1,24 @@
+# file = open('tasks.txt')
+# contents = file.read()
+#
+# print(contents)
+# file.close()
+
+with open("tasks.txt") as file:
+    contents=file.read()
+    print(contents)
+#
+# with open("tasks.txt",mode='w') as file:
+#     file.write("Written using files")
+#     print(contents)    //This deletes all the prev text inside the file
+
+
+with open("tasks.txt",mode='a') as file:
+    file.write("\nWritten using files")
+    print(contents)
+
+# Creating a file using w mode
+
+with open("new_file.txt",mode='w') as file:
+    file.write("I Created a file using python")
+
