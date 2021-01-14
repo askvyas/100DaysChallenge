2fee89a4d4e9b286596c6908b25744e2387076d3
askvyas <vyasakondi009@gmail.com>
2021-01-14 10:32:38 +0530

    Day24 Mail Merge Challenge complete

--- Day24/mail-merge-project-start/main.py
+++ Day24/mail-merge-project-start/main.py
@@ -1,8 +1,23 @@
-#TODO: Create a letter using starting_letter.docx 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("input/Names/invited_names.txt") as names:
+    for f in names:
+        name=f.strip()
+        print(name)
+        with open("input/Letters/starting_letter.txt") as letter:
+            new_text = letter.read().replace("[name]", name)
+
+        with open(f"output/ReadyToSend/{name}.txt", "w") as op:
+            op.write(new_text)
+
+
+
+
+
