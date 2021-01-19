1a84b09e6e32eef3670adca39d7f9706b84ed87b
askvyas <vyasakondi009@gmail.com>
2021-01-19 23:58:13 +0530

    US State Game Functionally working

--- Day25/US_State_Quiz/main.py
+++ Day25/US_State_Quiz/main.py
@@ -10,13 +10,37 @@ screen.bgpic(picname='blank_states_img.gif')
 tut_gm.hideturtle()
 tut_gm.penup()
 # importing data
-df=p
+df = pd.read_csv("50_states.csv")
+states = df["state"].to_list()
+
+# Create a list of xcor and ycor from state name from pandas csv
+loc=[]
+for st in states:
+    x_cor=int(df[df["state"]==st]['x'])
+    y_cor=int(df[df["state"]==st]['y'])
+    loc.append((x_cor,y_cor))
+
+
+# Create a dictonary with state names and x_cor and y_cor
+dict= {
+    "State":states,
+    "Location":loc
+}
+# Export this dataframe into a pandas
 
 
 # Game in a loop
 game_is_on=True
 while game_is_on:
-    screen.textinput("State Name", "Enter The State name")
+    state = screen.textinput("State Name", "Enter The State name")
+    if state in states:
+        ind=states.index(state)
+        tut_gm.goto(loc[ind])
+        tut_gm.write(state, True, align="center")
+
+    else:
+        break
+
 
 
 
