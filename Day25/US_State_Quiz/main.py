1bebed3b4f1ae9a2a89f23378bcd0a952d2ec1d4
askvyas <vyasakondi009@gmail.com>
2021-01-19 23:24:58 +0530

    US State Game Turtle code added

--- Day25/US_State_Quiz/main.py
+++ Day25/US_State_Quiz/main.py
@@ -1,7 +1,24 @@
 # Game of US State Quiz
 from turtle import Turtle,Screen
+import  pandas as pd
 
+
+# Turtle Setup
 tut_gm=Turtle()
 screen=Screen()
+screen.bgpic(picname='blank_states_img.gif')
+tut_gm.hideturtle()
+tut_gm.penup()
+# importing data
+df=p
+
+
+# Game in a loop
+game_is_on=True
+while game_is_on:
+    screen.textinput("State Name", "Enter The State name")
+
+
+
 
 screen.exitonclick()
