ee06b4bde6e4d6b9953d437a81783a9742d19f84
askvyas <vyasakondi009@gmail.com>
2021-01-20 22:20:33 +0530

    Day 25 Ex1 Added

--- Day26/prc.py
+++ Day26/prc.py
@@ -1,1 +1,9 @@
 # list comprehensions
+# numbers=[1,2,3]
+# new_list=[item+1 for item in numbers]
+# print(new_list)
+
+# Excersise one
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+sq_nums=[n**2 for n in numbers]
+print(sq_nums)
