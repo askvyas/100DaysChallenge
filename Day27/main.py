b5765babf0cb6cf97a0077c4386775e095ece0c1
askvyas <vyasakondi009@gmail.com>
2021-01-21 22:25:19 +0530

    Day27 Added

---
+++ Day27/main.py
@@ -0,0 +1,20 @@
+# Tkinter GUI Programming
+import tkinter
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500,height=300)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
