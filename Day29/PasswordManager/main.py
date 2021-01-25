e588c5558cfa21810ceeaca8520a82f8c835b39c
askvyas <vyasakondi009@gmail.com>
2021-01-25 23:03:05 +0530

    PasswordManager Picture added

--- Day29/PasswordManager/main.py
+++ Day29/PasswordManager/main.py
@@ -1,5 +1,23 @@
+# Imports
+from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window=Tk()
+window.title("Password Manager")
+window.config(padx=20,pady=20)
+# Image Widget
+logo_img=PhotoImage(file="logo.png")
+canvas=Canvas(width=200,height=200,highlightthickness=1)
+canvas.create_image(100,100,image=logo_img)
+canvas.grid(column=1,row=1)
+# using Column Span
+# canvas.grid(row=2,col=0,columnspan=2)
+
+#
+
+window.mainloop()
